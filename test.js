0890a5d43f290da89ed2ffc2424ab16172a03736
ZiadHegazy99 <ziad.hegazy@student.guc.edu.eg>
2024-08-11 18:21:01 +0300

    aa

---
+++ test.js
@@ -0,0 +1,12 @@
+// const jwt=require("jsonwebtoken")
+// var object={userId:"87033e74-dc2f-4672-87ba-6fdd0024d4d1"} //72678a7f-3922-4882-8a18-8dd0181213c2 //5177d092-b69f-4d6f-ae00-30ee549f2001
+// token=jwt.sign(object,"ziad1234aaaa&&&&&thisisasecretekeyaaa",{expiresIn:"24h"})
+// console.log(token)
+
+var n=7
+var c=0
+while(n>=0){
+    n=n-2
+    c=c+n-2
+}
+console.log(c)
